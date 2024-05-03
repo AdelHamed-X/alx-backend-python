39e0b6df045298b3f70f501801ed6749f44e18b4
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-05-03 16:41:34 +0300

    Task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python3
-""" Task 7 """
+
 from typing import Tuple, Union
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ Task 7 """
-    return tuple(k, int(v * v))
+    return (k, int(v * v))
