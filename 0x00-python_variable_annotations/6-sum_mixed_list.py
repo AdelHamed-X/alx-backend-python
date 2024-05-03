e5e00c61b0f537c1e52501be3a16959c920af5ac
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-05-03 16:08:41 +0300

    Task 6

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,6 +3,6 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int | float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int | float]]) -> float:
     """ Task 5 """
     return float(sum(mxd_lst))
