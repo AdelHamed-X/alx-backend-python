dfd47cb636b656083576b2b6cf52bb214cc8df0b
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-05-03 19:03:01 +0300

    Task 10

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Task 10 """
+from typing import Any, Union, Sequence
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
