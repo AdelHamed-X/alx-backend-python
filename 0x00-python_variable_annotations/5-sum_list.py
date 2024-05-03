14567c152de7cc89da09158b0b175a455426ec44
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-05-03 16:02:46 +0300

    Task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ Task 5 """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ Task 5 """
     return float(sum(input_list))
