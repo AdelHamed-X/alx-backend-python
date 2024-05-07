a4a9701bc5ef8fcabac37b95f423077ed1b6772c
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-05-08 02:58:19 +0300

    Adding Task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(int: max_delay=10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """ Task 0 """
     random_int = random.uniform(0, max_delay)
     test = await asyncio.sleep(random_int)
