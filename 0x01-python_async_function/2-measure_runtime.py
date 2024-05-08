3d71133e2a85062a04e0ac9ac613b8c58c8f0824
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-05-08 03:29:43 +0300

    Adding Task 2

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Task 2 """
+import asyncio
+import time
+from typing import List
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, delay_max: int) -> float:
+    """ Task 2 """
+    start = time.time()
+
+    asyncio.run(wait_n(n, delay_max))
+
+    end = time.time()
+    elapsed_time = end - start
+
+    return elapsed_time / n
