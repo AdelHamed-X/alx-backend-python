f6b58e7592f86b0479d10f32119b73472e28c4b5
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-05-08 03:10:17 +0300

    Adding Task 1

---
+++ 0x01-python_async_function/1-concuurent_coroutines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Task 1 """
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ Task 1 """
+    new_list = []
+    for ind in range(n):
+        new_list.append(await wait_random(max_delay))
+    return new_list
